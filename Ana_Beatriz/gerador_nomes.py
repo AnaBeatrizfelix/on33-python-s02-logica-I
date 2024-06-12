0f47a27e1992288b0cbb3e61bd333afe468c76b2
Ana Beatriz <biaibc23@gmail.com>
2024-06-12 20:02:08 -0300

    primeiro commit

---
+++ Ana_Beatriz/gerador_nomes.py
@@ -0,0 +1,6 @@
+cor = input("Digite sua cor favorita: ")
+animal = input("Difgite um animal de sua preferencia: ")
+sobremesa = input("Digite a sobremesa que mais gosta: ")
+
+
+print(f"SEU NOME DIVERTIDO É: ")
