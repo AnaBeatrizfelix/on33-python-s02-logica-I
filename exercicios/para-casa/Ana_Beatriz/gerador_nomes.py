dffacff2ad68cd8f003d8caf6e9d9b1453acc22e
Ana Beatriz <biaibc23@gmail.com>
2024-06-12 20:55:07 -0300

    criando README.md

--- exercicios/para-casa/Ana_Beatriz/gerador_nomes.py
+++ exercicios/para-casa/Ana_Beatriz/gerador_nomes.py
@@ -3,14 +3,15 @@ animal = input("Difgite um animal de sua preferencia: ")
 sobremesa = input("Digite a sobremesa que mais gosta: ")
 
 
-print(f"SEU NOME DIVERTIDO É: " + cor, animal, sobremesa)
+print(f"SEU NOME DIVERTIDO É: " + cor.upper(), animal.upper(), sobremesa.upper())
 
 #aproveitando o dia dos namorados, vamos ser criativos
+
 valor1 = int(input("Digite um numero:"))
 valor2 = int(input("Digite outro numero:"))
 
-lista1 =[3,9,5]
-lista2 =[1,8,4]
+lista1 =[3,9,5,7,0]
+lista2 =[1,8,40,78]
 
 if valor1 in lista1 and valor2 in lista2:
   print(f"te amo")
